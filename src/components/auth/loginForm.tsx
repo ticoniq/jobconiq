0e678498b6436f47ba691fa010430fc38dd33c6a
Ticoniq <okolietochi@gmail.com>
2024-06-22 23:00:41 -0700

    chore: Update login form with lazy loading for images and remove unused code

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -17,17 +17,12 @@ import {
   FormLabel,
   FormMessage
 } from "@/components/ui/form";
-import { useSearchParams } from 'next/navigation';
 import CustomLink from '@/components/ui/custom-link';
 import { FormError } from '@/components/FormError';
 import { FormSuccess } from '@/components/FormSuccess';
+import { UseSearchParamsWrapper } from '@/hooks/UseSearchParamsWrapper';
 
 export function LoginForm() {
-  const searchParams = useSearchParams();
-  const urlError =
-    searchParams.get("error") === "OAuthAccountNotLinked"
-      ? "Email already in use with other provider!"
-      : "";
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
@@ -62,81 +57,85 @@ export function LoginForm() {
   };
 
   return (
-    <Suspense>
-      <div className="flex flex-col gap-6 ">
-        <SocialLogin />
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t-2 border-neutrals-400" />
-          </div>
-          <div className="relative flex justify-center text-md text-muted">
-            <span className="bg-background px-4 text-muted-foreground">
-              Or login with email
-            </span>
-          </div>
+    <div className="flex flex-col gap-6 ">
+      <SocialLogin />
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t-2 border-neutrals-400" />
         </div>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-6"
-            noValidate
-          >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl className="font-semibold text-base text-neutrals-800">
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="johndoe@example.com"
-                      type="email"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex justify-between items-center">
-                    <FormLabel>Password</FormLabel>
-                    <CustomLink
-                      href="/forgot-password"
-                      className="text-brand-primary font-semibold"
-                      textarea={'Forgot your password?'}
-                      divClassName="border-brand-primary border-b-2"
-                    />
-                  </div>
-                  <FormControl className="font-semibold text-base text-neutrals-800">
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="********"
-                      type="password"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormError message={error || urlError} />
-            <FormSuccess message={success} />
-            <Button className="w-full" disabled={isPending}>
-              {isPending && (
-                <ImSpinner8 className="mr-2 h-4 w-4 animate-spin" />
-              )}
-              Login
-            </Button>
-          </form>
-        </Form>
-
+        <div className="relative flex justify-center text-md text-muted">
+          <span className="bg-background px-4 text-muted-foreground">
+            Or login with email
+          </span>
+        </div>
       </div>
-    </Suspense>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6"
+          noValidate
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl className="font-semibold text-base text-neutrals-800">
+                  <Input
+                    {...field}
+                    disabled={isPending}
+                    placeholder="johndoe@example.com"
+                    type="email"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex justify-between items-center">
+                  <FormLabel>Password</FormLabel>
+                  <CustomLink
+                    href="/forgot-password"
+                    className="text-brand-primary font-semibold"
+                    textarea={'Forgot your password?'}
+                    divClassName="border-brand-primary border-b-2"
+                  />
+                </div>
+                <FormControl className="font-semibold text-base text-neutrals-800">
+                  <Input
+                    {...field}
+                    disabled={isPending}
+                    placeholder="********"
+                    type="password"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Suspense fallback={<div>Loading...</div>}>
+            <UseSearchParamsWrapper>
+              {({ urlError }) => (
+                <FormError message={error || urlError} />
+              )}
+            </UseSearchParamsWrapper>
+          </Suspense>
+          <FormSuccess message={success} />
+          <Button className="w-full" disabled={isPending}>
+            {isPending && (
+              <ImSpinner8 className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Login
+          </Button>
+        </form>
+      </Form>
+
+    </div>
   )
 }
