e1d77d639b0ca42de10fc1968d1b2811eac86afb
Ticoniq <okolietochi@gmail.com>
2024-06-22 22:43:38 -0700

    chore: Add lazy loading for images and update login form

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -1,11 +1,10 @@
 "use client"
 import * as z from 'zod';
-import { useState, useTransition } from 'react';
+import { Suspense, useState, useTransition } from 'react';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { LoginSchema } from '@/lib/validation/auth-validation';
 import { ImSpinner8 } from "react-icons/im";
-import { FcGoogle } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { login } from "@/actions/developer/auth/login";
@@ -63,79 +62,81 @@ export function LoginForm() {
   };
 
   return (
-    <div className="flex flex-col gap-6 ">
-      <SocialLogin />
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t-2 border-neutrals-400" />
+    <Suspense>
+      <div className="flex flex-col gap-6 ">
+        <SocialLogin />
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t-2 border-neutrals-400" />
+          </div>
+          <div className="relative flex justify-center text-md text-muted">
+            <span className="bg-background px-4 text-muted-foreground">
+              Or login with email
+            </span>
+          </div>
         </div>
-        <div className="relative flex justify-center text-md text-muted">
-          <span className="bg-background px-4 text-muted-foreground">
-            Or login with email
-          </span>
-        </div>
-      </div>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-6"
-          noValidate
-        >
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl className="font-semibold text-base text-neutrals-800">
-                  <Input
-                    {...field}
-                    disabled={isPending}
-                    placeholder="johndoe@example.com"
-                    type="email"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <div className="flex justify-between items-center">
-                  <FormLabel>Password</FormLabel>
-                  <CustomLink
-                    href="/forgot-password"
-                    className="text-brand-primary font-semibold"
-                    textarea={'Forgot your password?'}
-                    divClassName="border-brand-primary border-b-2"
-                  />
-                </div>
-                <FormControl className="font-semibold text-base text-neutrals-800">
-                  <Input
-                    {...field}
-                    disabled={isPending}
-                    placeholder="********"
-                    type="password"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormError message={error || urlError} />
-          <FormSuccess message={success} />
-          <Button className="w-full" disabled={isPending}>
-            {isPending && (
-              <ImSpinner8 className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Login
-          </Button>
-        </form>
-      </Form>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6"
+            noValidate
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl className="font-semibold text-base text-neutrals-800">
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="johndoe@example.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="flex justify-between items-center">
+                    <FormLabel>Password</FormLabel>
+                    <CustomLink
+                      href="/forgot-password"
+                      className="text-brand-primary font-semibold"
+                      textarea={'Forgot your password?'}
+                      divClassName="border-brand-primary border-b-2"
+                    />
+                  </div>
+                  <FormControl className="font-semibold text-base text-neutrals-800">
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="********"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormError message={error || urlError} />
+            <FormSuccess message={success} />
+            <Button className="w-full" disabled={isPending}>
+              {isPending && (
+                <ImSpinner8 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Login
+            </Button>
+          </form>
+        </Form>
 
-    </div>
+      </div>
+    </Suspense>
   )
 }
