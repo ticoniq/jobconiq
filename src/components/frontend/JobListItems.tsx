9224d998d3ff93a3aed16c8d75c6fe0874effa18
Ticoniq <okolietochi@gmail.com>
2024-06-28 21:03:37 +0100

    chore: Update JobListItems component with breadcrumb and avatar

--- src/components/frontend/JobListItems.tsx
+++ src/components/frontend/JobListItems.tsx
@@ -6,32 +6,44 @@ import Link from "next/link";
 import { formatMoney } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Job } from "@prisma/client";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "../ui/breadcrumb";
+import { Dot } from "lucide-react";
 
 interface JobListItemProps {
   job: Job;
 }
 
 
-function JobListItems({job}: JobListItemProps) {
+function JobListItems({ job }: JobListItemProps) {
   return (
     <Card x-chunk="dashboard-01-chunk-5" className="rounded-none p-4 bg-inherit dark:border-neutrals-900 border-border">
       <CardContent className="p-0 space-y-5 flex flex-col items-start justify-between sm:flex-row sm:space-y-0">
         <div className="flex flex-row justify-start items-start gap-5">
-          <Avatar className="h-14 w-14 sm:flex">
+          <Avatar className="h-14 w-14 sm:flex rounded-none">
             <AvatarImage
               src={job.companyLogoUrl || "/avatars/01.png"}
               className="rounded-none"
               alt="Avatar"
             />
-            <AvatarFallback>JC</AvatarFallback>
+            <AvatarFallback className="rounded-none">JC</AvatarFallback>
           </Avatar>
-          <div className="grid gap-1 text-start">
+          <div className="grid gap-y-2  text-start">
             <p className="text-xl font-semibold leading-none">
               {job.title}
             </p>
-            <p className="text-base text-muted-foreground">
-              {job.location}
-            </p>
+            <Breadcrumb className="font-normal">
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink>{job.companyName}</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator>
+                  <Dot />
+                </BreadcrumbSeparator>
+                <BreadcrumbItem>
+                  <BreadcrumbLink>{job.location || "Worldwide"}</BreadcrumbLink>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
             <div className="space-x-2">
               <Badge variant={"secondary"}>{job.type}</Badge>
               <Badge variant={"warning"}>{job.locationType}</Badge>
