11190d11cffe526b0ba95e670ce392737aff41d6
Ticoniq <okolietochi@gmail.com>
2024-06-28 21:59:21 +0100

    chore: Update next.config.mjs with environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    WEBSITE_URL: process.env.WEBSITE_URL,
+    WEBSITE_EMAIL: process.env.WEBSITE_EMAIL,
+  },
+};
 
 export default nextConfig;
