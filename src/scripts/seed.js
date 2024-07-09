746d13600f759f9f308fc0a46a58d890b82e78f5
Ticoniq <okolietochi@gmail.com>
2024-07-09 04:18:46 +0100

    feat: Add SocialForm component for managing company social links
    This commit adds a new component called SocialForm, which allows users to manage their company's social links. The component is implemented in the src/app/(user)/company/settings/SocialForm.tsx file.
    
    The SocialForm component uses various UI components such as Form, FormControl, FormField, and Input to create a form for entering social links for platforms like Instagram, Twitter, Facebook, LinkedIn, and YouTube. The form includes validation using the companySocialSchema from the company-validation module.
    
    This feature enhancement improves the user experience by providing a dedicated form for managing company social links in the company settings.

--- src/scripts/seed.js
+++ src/scripts/seed.js
@@ -24,7 +24,6 @@ main()
     await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error("Error while seeding database:", e);
     await prisma.$disconnect();
     process.exit(1);
   });
