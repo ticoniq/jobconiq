4221d40d107df92ddb0177248ff35280d64cbd03
Ticoniq <okolietochi@gmail.com>
2024-07-11 04:43:51 +0100

    feat: Add SocialForm component for managing company social links

--- src/app/(user)/developer/profile/page.tsx
+++ src/app/(user)/developer/profile/page.tsx
@@ -1,13 +1,43 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
 import { currentUser } from "@/lib/auths"
-import Link from "next/link"
+import React, { cache, Fragment } from "react"
+import prisma from "@/lib/prisma"
 import Image from "next/image"
-import React from "react"
+import Link from "next/link"
+import { Github, Globe, Languages, Linkedin, Mail, PenSquare, Smartphone } from "lucide-react"
+import Markdown from "@/components/Markdown"
+import { Experiences } from "./Experiences"
+import { Educations } from "./Educations"
+
+const getCachedUserDetails = cache(async (uid: string) => {
+  return await prisma.developer.findUnique({
+    where: {
+      userId: uid,
+    }
+  });
+})
 
 async function ProfilePage() {
   const user = await currentUser();
+
+  if (!user) {
+    return <div>Please log in to view your profile.</div>;
+  }
+
+  const userDetails = await getCachedUserDetails(user?.id ?? "");
+
+  if (!userDetails) {
+    return <div>User details not found.</div>;
+  }
 
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-10">
@@ -17,26 +47,8 @@ async function ProfilePage() {
       <section className="grid gap-4 lg:grid-cols-2 xl:grid-cols-3">
         <div className="xl:col-span-2 space-y-4">
           <Card className="bg-transparent rounded-none border border-brand-secondary">
-            {/* <CardHeader className="flex flex-row items-center">
-              <div className="grid gap-2">
-                <CardTitle>Transactions</CardTitle>
-                <CardDescription>
-                  Recent transactions from your store.
-                </CardDescription>
-              </div>
-              <Button asChild size="sm" className="ml-auto gap-1">
-                <Link href="#">
-                  View All
-                </Link>
-              </Button>
-            </CardHeader> */}
             <CardHeader className="p-0">
               <div className="relative pt-16 pb-16">
-                {/* <Image
-                  src="https://pagedone.io/asset/uploads/1705473378.png"
-                  alt="cover-image"
-                  className="w-full absolute top-0 left-0 z-0 h-36"
-                /> */}
                 <div className="w-full absolute top-0 left-0 z-0 h-36 bg-gradient-to-r from-teal-400 to-yellow-200" />
                 <div className="px-6 md:px-8">
                   <div className="flex items-center justify-center sm:justify-start relative z-10 mb-5">
@@ -53,117 +65,146 @@ async function ProfilePage() {
             </CardContent>
           </Card>
           <Card className="bg-transparent rounded-none border border-brand-secondary">
-            <CardHeader className="flex flex-row items-center">
-              <div className="grid gap-2">
-                <CardTitle>Transactions</CardTitle>
-                <CardDescription>
-                  Recent transactions from your store.
-                </CardDescription>
-              </div>
-              <Button asChild size="sm" className="ml-auto gap-1">
-                <Link href="#">
-                  View All
-                </Link>
-              </Button>
+            <CardHeader>
+              <CardTitle className="font-clash">About Me</CardTitle>
             </CardHeader>
             <CardContent>
-              hola mundo
+              {userDetails?.bio && <Markdown>{userDetails?.bio}</Markdown>}
             </CardContent>
           </Card>
           <Card className="bg-transparent rounded-none border border-brand-secondary">
-            <CardHeader className="flex flex-row items-center">
-              <div className="grid gap-2">
-                <CardTitle>Transactions</CardTitle>
-                <CardDescription>
-                  Recent transactions from your store.
-                </CardDescription>
-              </div>
-              <Button asChild size="sm" className="ml-auto gap-1">
-                <Link href="#">
-                  View All
-                </Link>
-              </Button>
+            <CardHeader className="flex flex-row items-center justify-between">
+              <CardTitle className="font-clash">Experiences</CardTitle>
+              <Experiences />
             </CardHeader>
             <CardContent>
-              hola mundo
+              <CardDescription>
+                Coming soon...
+              </CardDescription>
             </CardContent>
           </Card>
           <Card className="bg-transparent rounded-none border border-brand-secondary">
-            <CardHeader className="flex flex-row items-center">
-              <div className="grid gap-2">
-                <CardTitle>Transactions</CardTitle>
-                <CardDescription>
-                  Recent transactions from your store.
-                </CardDescription>
-              </div>
-              <Button asChild size="sm" className="ml-auto gap-1">
-                <Link href="#">
-                  View All
-                </Link>
-              </Button>
+            <CardHeader className="flex flex-row items-center justify-between">
+              <CardTitle className="font-clash">Educations</CardTitle>
+              <Educations />
             </CardHeader>
             <CardContent>
-              hola mundo
+              <CardDescription>
+                Coming soon...
+              </CardDescription>
             </CardContent>
           </Card>
           <Card className="bg-transparent rounded-none border border-brand-secondary">
-            <CardHeader className="flex flex-row items-center">
-              <div className="grid gap-2">
-                <CardTitle>Transactions</CardTitle>
-                <CardDescription>
-                  Recent transactions from your store.
-                </CardDescription>
-              </div>
-              <Button asChild size="sm" className="ml-auto gap-1">
-                <Link href="#">
-                  View All
-                </Link>
-              </Button>
+            <CardHeader>
+              <CardTitle className="font-clash">Skills</CardTitle>
             </CardHeader>
             <CardContent>
-              hola mundo
+              <div className="text-sm leading-6 flex flex-wrap gap-2">
+                {userDetails?.skills[0] ? (
+                  <>
+                    {userDetails.skills.map((skill) => (
+                      <Badge
+                        key={skill}
+                        variant="info"
+                        className="rounded-none whitespace-nowrap text-ellipsis px-2 text-blue-700"
+                      >
+                        {skill}
+                      </Badge>
+                    ))}
+                  </>
+                ) : (
+                  <>{"No bio available."}</>
+                )}
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="bg-transparent rounded-none border border-brand-secondary">
+            <CardHeader>
+              <CardTitle className="font-clash">Portfolios</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>
+                Coming soon...
+              </CardDescription>
             </CardContent>
           </Card>
         </div>
         <aside className="space-y-4">
           <Card className="bg-transparent rounded-none border border-brand-secondary">
             <CardHeader>
-              <CardTitle>Recent Sales</CardTitle>
+              <CardTitle className="font-clash">Additional Details</CardTitle>
             </CardHeader>
             <CardContent className="grid gap-8">
-              <div className="flex items-center gap-4">
-
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    Olivia Martin
+              <div className="flex gap-4">
+                <Mail />
+                <aside className="flex flex-col">
+                  <span className="text-base leading-6">Email</span>
+                  <p className="text-sm leading-6 break-all">
+                    {user?.email ? user.email : "Not specified"}
                   </p>
-                  <p className="text-sm text-muted-foreground">
-                    olivia.martin@email.com
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+$1,999.00</div>
+                </aside>
               </div>
-
+              <div className="flex gap-4">
+                <Smartphone />
+                <aside className="flex flex-col">
+                  <span className="text-base leading-6">Phone</span>
+                  <p className="text-sm leading-6 break-all">
+                    {userDetails?.phone ? userDetails.phone : "Not specified"}
+                  </p>
+                </aside>
+              </div>
+              <div className="flex gap-4">
+                <Languages />
+                <aside className="flex flex-col">
+                  <span className="text-base leading-6">Languages</span>
+                  {userDetails?.languages?.length > 0 ? (
+                    <>
+                      {userDetails.languages.map((skill, index, array) => (
+                        <Fragment key={index}>
+                          {skill}
+                          {index < array.length - 1 && ", "}
+                        </Fragment>
+                      ))}
+                    </>
+                  ) : (
+                    <p>{"No skills available."}</p>
+                  )}
+                </aside>
+              </div>
             </CardContent>
           </Card>
           <Card className="bg-transparent rounded-none border border-brand-secondary">
             <CardHeader>
-              <CardTitle>Recent Sales</CardTitle>
+              <CardTitle className="font-clash">Social Links</CardTitle>
             </CardHeader>
             <CardContent className="grid gap-8">
-              <div className="flex items-center gap-4">
-
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    Olivia Martin
+              <div className="flex gap-4">
+                <Github />
+                <aside className="flex flex-col">
+                  <span className="text-base leading-6">Github</span>
+                  <p className="text-sm leading-6 break-all">
+                    {userDetails?.github ? userDetails.github : "Not specified"}
                   </p>
-                  <p className="text-sm text-muted-foreground">
-                    olivia.martin@email.com
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+$1,999.00</div>
+                </aside>
               </div>
-
+              <div className="flex gap-4">
+                <Linkedin />
+                <aside className="flex flex-col">
+                  <span className="text-base leading-6">LinkedIn</span>
+                  <p className="text-sm leading-6 break-all">
+                    {userDetails?.linkedin ? userDetails.linkedin : "Not specified"}
+                  </p>
+                </aside>
+              </div>
+              <div className="flex gap-4">
+                <Globe />
+                <aside className="flex flex-col">
+                  <span className="text-base leading-6">Portfolio</span>
+                  <p className="text-sm leading-6 break-all">
+                    {userDetails?.website ? userDetails.website : "Not specified"}
+                  </p>
+                </aside>
+              </div>
             </CardContent>
           </Card>
         </aside>
