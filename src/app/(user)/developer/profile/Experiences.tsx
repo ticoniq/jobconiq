0606d153607762eb789510588ff150426ba82227
Ticoniq <okolietochi@gmail.com>
2024-07-11 04:43:19 +0100

    feat: Add Educations and Experiences components for developer profile
    This commit adds the Educations and Experiences components to the developer profile. These components are responsible for displaying the education and work experience sections respectively. The components utilize the Drawer component from the UI library to provide a collapsible view for each section. This feature enhancement improves the developer profile by allowing users to showcase their educational background and work experience in a structured and organized manner.

---
+++ src/app/(user)/developer/profile/Experiences.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Button } from "@/components/ui/button"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { PenSquare } from "lucide-react";
+
+export function Experiences() {
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant={"outline"} className="h-7 px-2 py-2">
+          <PenSquare className="h-4 w-4" />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Move Goal</DrawerTitle>
+            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 pb-0">
+
+          </div>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
